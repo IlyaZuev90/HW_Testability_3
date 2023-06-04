4b806fb18f34f1a5f83957645ef52fc3715364c4
Ilya_Zuev_90 <god_of_iwowi@mail.ru>
2023-06-04 21:19:52 +0400

    Renamed variables using camelCase

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,14 +1,14 @@
 public class CreditPaymentService {
-    public int calculate(int amountOfCredit_ruble, double creditTerm_months, double interestRate_percentage) {
+    public int calculate(int amountOfCreditRuble, double creditTermMonths, double interestRatePercentage) {
 
         int annuityPayment;
-        double interestRate_perMonth;
+        double interestRatePerMonth;
         double annuityRatio;
 
 
-        interestRate_perMonth = (interestRate_percentage / 12) / 100;
-        annuityRatio = interestRate_perMonth * (Math.pow((1 + interestRate_perMonth), creditTerm_months) / (Math.pow((1 + interestRate_perMonth), creditTerm_months) - 1));
-        annuityPayment = (int) (amountOfCredit_ruble * annuityRatio);
+        interestRatePerMonth = (interestRatePercentage / 12) / 100;
+        annuityRatio = interestRatePerMonth * (Math.pow((1 + interestRatePerMonth), creditTermMonths) / (Math.pow((1 + interestRatePerMonth), creditTermMonths) - 1));
+        annuityPayment = (int) (amountOfCreditRuble * annuityRatio);
 
 
         return annuityPayment;
